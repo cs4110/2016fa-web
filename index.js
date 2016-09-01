1f2ba6d005cb649460ca74817fe621bc68ef6870
Adrian Sampson <adrian@radbox.org>
2016-09-01 11:53:50 -0400

    A little more skeleton

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+var Metalsmith  = require('metalsmith');
+var markdown    = require('metalsmith-markdown');
+var layouts     = require('metalsmith-layouts');
+
 Metalsmith(__dirname)
+  .source('.')
+  .destination('./build')
   .use(markdown())
   .use(layouts('handlebars'))
   .build(function(err) {
