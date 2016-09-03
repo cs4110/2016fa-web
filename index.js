3b7c9335abf327fbcd413ea8252f95d71ab18a9d
Adrian Sampson <adrian@radbox.org>
2016-09-03 17:42:59 -0400

    Stylesheet

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var Metalsmith  = require('metalsmith');
 var markdown    = require('metalsmith-markdown');
 var layouts     = require('metalsmith-layouts');
+var sass        = require('metalsmith-sass');
 
 Metalsmith(__dirname)
   .source('./src')
   .destination('./build')
   .use(markdown())
   .use(layouts('handlebars'))
+  .use(sass())
   .build(function(err) {
     if (err) throw err;
   });
