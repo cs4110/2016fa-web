ea3c94ddc63862403ce8b246fcc4e67c36ee8a9b
Adrian Sampson <adrian@radbox.org>
2016-09-03 21:28:25 -0400

    Course metadata

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
-.PHONY: build clean
+.PHONY: build clean view
 
 build: node_modules
 	npm run build
 
 clean:
 	rm -rf build
+
+view:
+	open build/index.html
 
 node_modules: package.json
 	npm install
--- index.js
+++ index.js
@@ -2,13 +2,17 @@ var Metalsmith  = require('metalsmith');
 var markdown    = require('metalsmith-markdown');
 var layouts     = require('metalsmith-layouts');
 var sass        = require('metalsmith-sass');
+var metadata    = require('metalsmith-metadata');
 
 Metalsmith(__dirname)
   .source('./src')
   .destination('./build')
   .use(markdown())
-  .use(layouts('handlebars'))
   .use(sass())
+  .use(metadata({
+    course: 'course.yaml'
+  }))
+  .use(layouts('handlebars'))
   .build(function(err) {
     if (err) throw err;
   });
