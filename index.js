6167795abf7b2c7a8b266084a6b07346e1b2fce7
Adrian Sampson <adrian@radbox.org>
2016-09-03 21:59:01 -0400

    Try adding nav

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ var markdown    = require('metalsmith-markdown');
 var layouts     = require('metalsmith-layouts');
 var sass        = require('metalsmith-sass');
 var metadata    = require('metalsmith-metadata');
+var relative    = require('metalsmith-relative');
+var collections = require('metalsmith-collections');
 
 Metalsmith(__dirname)
   .source('./src')
   .destination('./build')
+  .use(relative())
   .use(markdown())
   .use(sass())
+  .use(collections({
+    pages: '*.md'
+  }))
   .use(metadata({
     course: 'course.yaml'
   }))
